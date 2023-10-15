7f71e62b46ce5afaa22ca32a556a51774fb08a92
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2023-10-15 19:11:31 -0400

    Added create a book and ability to fetch list of books by author id, also created gitignore file to avoid conflicts with IDEs

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask, jsonify, request
 from commentsandratings import get_ratings, get_comments, get_average_rating
-from book_details import get_books
+from book_details import get_books_ISBN, create_book, get_books_author
 
 app = Flask(__name__)
 
 
 # get book by ISBN
 @app.get("/books/<book_isbn>")
-def retrieve_books(book_isbn):
-    data = get_books(book_isbn)
+def get_books(book_isbn):
+    data = get_books_ISBN(book_isbn)
     return data
+
+
+# get book by author ID
+@app.get("/books/author/<book_author_id>")
+def get_books_by_author_id(book_author_id):
+    data = get_books_author(book_author_id)
+    return data
+
+
+# create book
+@app.post("/create-book")
+def new_book():
+    return create_book()
 
 
 @app.route("/ratings", methods=["GET"])
--- book_details.py
+++ book_details.py
@@ -1,9 +1,9 @@
 from database import get_db
-from flask import jsonify
+from flask import jsonify, request
 
 
 #  GET BOOK BY ISBN
-def get_books(ISBN):
+def get_books_ISBN(ISBN):
     try:
         # connect to the database
         connection = get_db()
@@ -22,8 +22,6 @@ def get_books(ISBN):
         # close the cursor and connection
         cursor.close()
         connection.close()
-
-        print(type(data))
 
         # turn the data into a list of objects
         results = []
@@ -51,10 +49,102 @@ def get_books(ISBN):
         return jsonify({"error": str(e)})
 
 
+# GET BOOKS FROM SPECIFIC AUTHOR ID
+def get_books_author(author_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"SELECT * FROM Books WHERE author_id = {author_id}"
+
+        cursor.execute(query)
+
+        data = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        results = []
+
+        for row in data:
+            results.append(
+                {
+                    "Book_ID": row[0],
+                    "ISBN": row[1],
+                    "Title": row[2],
+                    "Description": row[3],
+                    "Price": row[4],
+                    "Author ID": row[5],
+                    "Genre": row[6],
+                    "Publisher": row[7],
+                    "Year Published": row[8],
+                    "Copies Sold": row[9],
+                }
+            )
+
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
 # CREATE A BOOK
+def create_book():
+    try:
+        # connect to the database
+        connection = get_db()
+
+        # cursor to interact with the database
+        cursor = connection.cursor()
+
+        data = request.json
+
+        title = data["title"]
+        publisher = data["publisher"]
+        copies_sold = data["copies_sold"]
+        genre = data["genre"]
+        price = data["price"]
+        isbn = data["isbn"]
+        year_published = data["year_published"]
+        description = data["description"]
+
+        query = """
+            INSERT INTO Books 
+            (isbn, book_name, description, price, genre, publisher, year_published, copies_sold) 
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+        params = (
+            isbn,
+            title,
+            description,
+            price,
+            genre,
+            publisher,
+            year_published,
+            copies_sold,
+        )
+
+        cursor.execute(query, params)
+
+        connection.commit()
+
+        book = {
+            "title": data["title"],
+            "publisher": data["publisher"],
+            "copies_sold": data["copies_sold"],
+            "genre": data["genre"],
+            "price": data["price"],
+            "isbn": data["isbn"],
+            "year_published": data["year_published"],
+            "description": data["description"],
+        }
+
+        return jsonify(book), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
 # CREATE AN AUTHOR
-
-
-# GET BOOKS FROM SPECIFIC AUTHOR
