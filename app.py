04d9a15cbcccf0ae5b0147a1a57639a84d15639d
Angelica Moreno <angelica.mxrenx@gmail.com>
2023-11-26 20:16:26 -0500

    final debugs and modifications made to code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from commentsandratings import get_ratings, get_comments, get_average_rating
 from book_details import get_books_ISBN, create_book, get_books_author
 from users import get_users, get_user, create_user
-from wishlistmanagement import list_books_in_wishlist
+from wishlistmanagement import new_wishlist, add_book, delete_book, list_books
 
 app = Flask(__name__)
 
@@ -58,9 +58,21 @@ def find_user(username):
 def new_user():
     return create_user()
 
-@app.route('/wishlist/<wishlist_id>', methods=['GET'])
-def new_wishlist(wishlist_id):
-    return list_books_in_wishlist(wishlist_id)
+
+@app.route('/wishlist/<user_id>/<wishlist_name>/<book_id>', methods=['POST'])
+def create_wishlist(user_id, wishlist_name, book_id):
+    return new_wishlist(user_id, wishlist_name, book_id)
+
+@app.route('/add_wishlist/<user_id>/<wishlist_name>/<book_id>', methods=['POST'])
+def add_books(user_id, wishlist_name, book_id):
+    return add_book(user_id, wishlist_name, book_id)
+
+@app.route('/wishlist/<wishlist_name>/<book_id>', methods=['DELETE'])
+def delete_books(wishlist_name, book_id):
+    return delete_book(wishlist_name, book_id)
+@app.route('/wishlist/<book_id>', methods=['GET'])
+def list_bookss(book_id):
+    return list_books(book_id)
 
 if __name__ == "main":
     app.run(debug=True)
--- wishlistmanagement.py
+++ wishlistmanagement.py
@@ -1,98 +1,73 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from database import get_db
 
-app = Flask(__name__)
-
-
-
-@app.route('/wishlist/<int:wishlistId>', methods=['GET'])
-def get_wishlist(wishlistId):
-    if wishlistId in wishlist_data:
-        books_in_wishlist = wishlist_data[wishlistId]
-        return jsonify(books_in_wishlist)
-    else:
-        return jsonify({"error": "Wishlist not found"}), 404
-
-if __name__ == '__main__':
-    app.run()
-
-
-@app.route('/api/wishlist/add_book', methods=['POST'])
-def add_book_to_wishlist():
-    # Get data from the request
-    data = request.get_json()
-    book_id = data.get('BookId')
-    wishlist_id = data.get('WishlistId')
-
-    # Check if the book_id and wishlist_id are valid
-    if book_id is None or wishlist_id is None:
-        return jsonify({"error": "Invalid parameters"}), 400
-
-    # Add the book to the wishlist in your data store
-    if wishlist_id in wishlist_data:
-        wishlist_data[wishlist_id].append(book_id)
-    else:
-        wishlist_data[wishlist_id] = [book_id]
-
-    return jsonify({"message": "Book added to the wishlist"}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-@app.route('/api/wishlist/remove_and_add_to_cart', methods=['DELETE'])
-def remove_from_wishlist_and_add_to_cart():
-    # Get data from the request
-    data = request.get_json()
-    book_id = data.get('BookId')
-    wishlist_id = data.get('WishlistId')
-
-    # Check if the book_id and wishlist_id are valid
-    if book_id is None or wishlist_id is None:
-        return 'Invalid parameters', 400
-
-    # Remove the book from the wishlist in your data store
-    if wishlist_id in wishlist_data and book_id in wishlist_data[wishlist_id]:
-        wishlist_data[wishlist_id].remove(book_id)
-        # Add the book to the user's shopping cart
-        if wishlist_id in shopping_cart_data:
-            shopping_cart_data[wishlist_id].append(book_id)
-        else:
-            shopping_cart_data[wishlist_id] = [book_id]
-        return 'Book removed from wishlist and added to shopping cart', 204
-    else:
-        return 'Book or wishlist not found', 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-#list books in wishlist
-def list_books_in_wishlist():
-    # Get the WishlistId from the request
-    wishlist_id = request.args.get('WishlistId')
-
-    # Check if the wishlist_id is valid
-    if wishlist_id is None:
-        return jsonify({"error": "Invalid parameters"}), 400
-
-    # Retrieve the list of books associated with the specified WishlistId
-    if wishlist_id in wishlist_data:
-        books_in_wishlist = wishlist_data[wishlist_id]
-        return jsonify(books_in_wishlist), 200
-    else:
-        return jsonify({"error": "Wishlist not found"}), 404
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-@app.route('/wishlist/<wishlist_id>', methods=['GET'])
-def list_books_in_wishlist(wishlist_id):
+def new_wishlist(user_id, wishlist_name, book_id):
     try:
         connection = get_db()
 
         cursor = connection.cursor()
 
-        query = f"SELECT * FROM wishlist WHERE wishlist_id = {wishlist_id}"
+        query = f"""INSERT INTO wishlist VALUES (NULL, {user_id}, '{wishlist_name}', {book_id})"""
+
+        cursor.execute(query)
+
+        connection.commit()
+
+        cursor.close()
+
+        connection.close()
+
+        return jsonify({"message": "Wishlist created successfully"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def add_book(user_id, wishlist_name,book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"""INSERT INTO wishlist VALUES (NULL, {user_id}, '{wishlist_name}', {book_id})"""
+
+        cursor.execute(query)
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({"message": "Books added successfully"})
+
+    except Exception as e:
+            return jsonify({"error": str(e)})
+def delete_book(wishlist_name, book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"DELETE FROM wishlist WHERE book_id = {book_id} AND wishlist_name = '{wishlist_name}'"
+
+        cursor.execute(query)
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({"message": "Books deleted successfully"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def list_books(book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"SELECT * FROM wishlist WHERE book_id = {book_id}"
 
         cursor.execute(query)
 
