5fec3ba87c188fda4747d2c28ec9b9628f27d11b
glendy <gandreamesa@gmail.com>
2023-11-26 21:49:53 -0500

    Merging final code

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+<<<<<<< HEAD
 from flask_cors import CORS
 from commentsandratings import get_ratings, get_comments, get_average_rating, rate_books, comment
 from shoppingcart import get_subtotal, add_to_cart, get_Book_List, remove_book_from_cart
+=======
+from commentsandratings import get_ratings, get_comments, get_average_rating
+from book_details import get_books_ISBN, create_book, get_books_author
+from wishlistmanagement import new_wishlist, add_book, delete_book, list_books
+from users import get_users, get_user, create_user, update_user, add_credit_card
+>>>>>>> 06c7e0d8b5d81d4a9a54106dd044615b4f0e9960
 
 app = Flask(__name__)
 CORS(app)
@@ -54,5 +61,44 @@ def get_books_in_cart(UserId):
 def remove_book(BookID, UserId):
     return remove_book_from_cart(BookID, UserId)
 
+<<<<<<< HEAD
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+@app.route('/users/<username>', methods=['GET'])
+def find_user(username):
+    user = get_user(username)
+    return user
+
+
+@app.route('/users/create', methods=['POST'])
+def new_user():
+    return create_user()
+
+@app.route('/users/update/<username>', methods=["PUT", "PATCH"])
+def edit_user(username):
+    return update_user(username)
+
+@app.route('/users/<username>/creditcard/new', methods=["POST"])
+def new_credit_card(username):
+    return add_credit_card(username)
+
+
+@app.route('/wishlist/<user_id>/<wishlist_name>/<book_id>', methods=['POST'])
+def create_wishlist(user_id, wishlist_name, book_id):
+    return new_wishlist(user_id, wishlist_name, book_id)
+
+@app.route('/add_wishlist/<user_id>/<wishlist_name>/<book_id>', methods=['POST'])
+def add_books(user_id, wishlist_name, book_id):
+    return add_book(user_id, wishlist_name, book_id)
+
+@app.route('/wishlist/<wishlist_name>/<book_id>', methods=['DELETE'])
+def delete_books(wishlist_name, book_id):
+    return delete_book(wishlist_name, book_id)
+@app.route('/wishlist/<book_id>', methods=['GET'])
+def list_bookss(book_id):
+    return list_books(book_id)
+
+if __name__ == "main":
+    app.run(debug=True)
+>>>>>>> 06c7e0d8b5d81d4a9a54106dd044615b4f0e9960
--- wishlistmanagement.py
+++ wishlistmanagement.py
@@ -1,16 +1,99 @@
-from flask import jsonify, request
-from database import app, get_db
+from flask import Flask, jsonify, request
+from database import get_db
 
-wishlist = {}
+def new_wishlist(user_id, wishlist_name, book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"""INSERT INTO wishlist VALUES (NULL, {user_id}, '{wishlist_name}', {book_id})"""
+
+        cursor.execute(query)
+
+        connection.commit()
+
+        cursor.close()
+
+        connection.close()
+
+        return jsonify({"message": "Wishlist created successfully"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def add_book(user_id, wishlist_name,book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"""INSERT INTO wishlist VALUES (NULL, {user_id}, '{wishlist_name}', {book_id})"""
+
+        cursor.execute(query)
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({"message": "Books added successfully"})
+
+    except Exception as e:
+            return jsonify({"error": str(e)})
+def delete_book(wishlist_name, book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"DELETE FROM wishlist WHERE book_id = {book_id} AND wishlist_name = '{wishlist_name}'"
+
+        cursor.execute(query)
+
+        connection.commit()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify({"message": "Books deleted successfully"})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def list_books(book_id):
+    try:
+        connection = get_db()
+
+        cursor = connection.cursor()
+
+        query = f"SELECT * FROM wishlist WHERE book_id = {book_id}"
+
+        cursor.execute(query)
+
+        data = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        results = []
+
+        for row in data:
+            results.append(
+                {
+                    "wishlist_id": row[0],
+                    "wishlist_name": row[1],
+                    "user_id": row[2],
+                    "book_id": row[3],
+                }
+            )
+
+        return jsonify(results)
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 
-@app.route('/wishlist', methods = ['GET'])
-def get_wishlist():
-    connection = get_db
-    cursor = connection.cursor()
-
-    query = "SELECT * FROM wishlist"
-    cursor.execute(query)
-
-
+if __name__ == '__main__':
+    app.run(debug=True)
 
